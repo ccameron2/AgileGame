2aa32569193e9d99cc7ff2ea200cb037d8f02974
LukasBaliukonis567 <72207017+LukasBaliukonis567@users.noreply.github.com>
2021-03-06 16:37:28 +0000

    AIcontroller

---
+++ Source/AgileGame/AIControllerClass.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIControllerClass.h"
+
+void AAIControllerClass::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0); //Gets the player pawn 
+
+	if (PlayerPosition != nullptr)
+	{
+		RunBehaviorTree(PlayerPosition);
+	}
+
+	GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerPosition"), PlayerPawn->GetActorLocation()); //Sets the blackboard component called "PlayerPosition" and sets it as current players position
+}
+
+void AAIControllerClass::Tick(float DeltaTime)
+{
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0); //Gets the player pawn 
+
+	GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerPosition"), PlayerPawn->GetActorLocation()); //Sets the blackboard component called "PlayerPosition" and sets it as current players position
+
+	if (LineOfSightTo(PlayerPawn)) //Checks if the AI character has line of sight to the player pawn
+	{
+		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerPosition"), PlayerPawn->GetActorLocation()); //Sets the blackboard component called "PlayerPosition" and sets it as current players position
+	}
+	else
+	{
+		GetBlackboardComponent()->ClearValue(TEXT("PlayerPosition")); //Clears the current value of the player location 
+	}
+
+
+}
