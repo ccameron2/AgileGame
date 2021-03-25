f91897328e81f79d7a6af502b312d6dc24db8404
CCameron2 <ccameron2@outlook.com>
2021-03-25 00:19:13 +0000

    Implemented AI drivers on Level 1

--- Source/AgileGame/AIControllerClass.cpp
+++ Source/AgileGame/AIControllerClass.cpp
@@ -22,7 +22,6 @@ void AAIControllerClass::BeginPlay()
 void AAIControllerClass::Tick(float DeltaTime)
 {
 	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0); //Gets the player pawn 
-
 	GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerPosition"), PlayerPawn->GetActorLocation()); //Sets the blackboard component called "PlayerPosition" and sets it as current players position
 
 	if (LineOfSightTo(PlayerPawn)) //Checks if the AI character has line of sight to the player pawn
